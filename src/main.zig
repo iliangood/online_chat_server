a2db9579533c4cf3a1cc65a80dce0cad8301855f
iliangood <ilia191168@gmail.com>
2026-03-22 21:03:24 +0300

    Работа над обработкой пакетов

--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,67 @@ fn handleSigInt(sig: std.os.linux.SIG) callconv(.c) void {
     should_exit = true;
 }
 
+const Request_type = union(enum(u8)) {
+    send_msg: struct {
+        chat_id: u128,
+        len: u64,
+    },
+    get_msg: struct {
+        chat_id: u128,
+        mode: union(enum(u8)) {
+            all: void,
+            latest: struct { limit: u64 },
+            range: struct {
+                is_rev: bool,
+                start: u64,
+                end: u64,
+            },
+        },
+    },
+    get_info: enum {
+        msg_count,
+    },
+};
+
+const Request = struct {
+    type: Request_type,
+    data: ?[]u8,
+};
+const Request_vec = struct {
+    requests: []Request,
+    data: []u8,
+    connection: std.Io.net.Stream,
+};
+
+fn getMsg(io: std.Io, allocator: std.mem.Allocator, connection: std.Io.net.Stream) !Request_vec {
+    var buf_reader: [4096]u8 = undefined;
+    var reader = connection.reader(io, &buf_reader);
+    const msg = try reader.interface.allocRemaining(allocator, .unlimited);
+    if (msg.len < 2) {
+        return error{TooShortPacket};
+    }
+    const requests_count = @as(u16, @bitCast(msg[0..2]));
+    const requests_msg = msg[2..];
+    if (requests_msg.len < requests_count * @sizeOf(Request_type)) {
+        return error{TooShortPacket};
+    }
+    const requests = @as([]Request_type, requests_msg[0 .. requests_count * @sizeOf(Request_type)]);
+    var data = requests_msg[requests_count * @sizeOf(Request_type) ..];
+    var res_requests = try allocator.alloc(Request, requests_count);
+    for (requests, 0..) |request, i| {
+        const cur_buf = switch (request) {
+            .send_msg => |cur| curp: {
+                const cur_b = data[0..cur.len];
+                data = data[cur.len..];
+                break :curp cur_b;
+            },
+            else => null,
+        };
+        res_requests[i] = Request{ .type = request, .data = cur_buf };
+    }
+    return .{ .requests = res_requests, .data = msg, .connection = connection };
+}
+
 pub fn main(init: std.process.Init) !void {
     var act = std.posix.Sigaction{
         .handler = .{ .handler = handleSigInt },
@@ -16,36 +77,25 @@ pub fn main(init: std.process.Init) !void {
         .flags = 0,
     };
     std.posix.sigaction(std.posix.SIG.INT, &act, null);
+
     const io = init.io;
     var stdout = std.Io.File.stdout().writer(io, &.{});
     const writer = &stdout.interface;
-    // _ = writer;
+    _ = writer;
     const allocator = std.heap.smp_allocator;
-    var messages = try std.ArrayList(Messaege).initCapacity(allocator, 64);
 
+    var queue_mutex = std.Io.Mutex.init;
+    var queue = try std.Deque(Request_vec).initCapacity(allocator, 64);
+
+    var messages = try std.ArrayList(Messaege).initCapacity(allocator, 64);
+    _ = messages;
     const port: u16 = 44089;
     const address = std.Io.net.IpAddress{ .ip4 = .unspecified(port) };
-    var server = try address.listen(io, .{});
+    var server = try address.listen(io, .{ .reuse_address = true });
     defer server.deinit(io);
     while (!should_exit) {
-        const connection = try server.accept(io);
-        defer connection.close(io);
-        try writer.print("connected:{f}\n", .{connection.socket.address});
-        // var buffer: [1024]u8 = undefined;
-        var reader = connection.reader(io, &.{});
-        var id_buf: [16]u8 = undefined;
-        if (try reader.interface.readSliceShort(&id_buf) != 16) {
-            continue;
-        }
-        var message: Messaege = undefined;
-        message.chat_id = std.mem.readInt(u128, &id_buf, .little);
-        message.data = try reader.interface.allocRemaining(allocator, .unlimited);
-
-        try messages.append(allocator, message);
-        // try writer.print("msg:\"{s}\"\n", .{msg});
+        // server.accept(io: Io)
     }
-    for (messages.items) |*msg| {
-        try writer.print("{}:{s}\n", .{ msg.chat_id, msg.data });
-    }
-    try writer.print("end\n", .{});
+    // for (messages.items) |*msg| {}
+    // try writer.print("end\n", .{});
 }
